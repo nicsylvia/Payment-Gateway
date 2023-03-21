6ad3e00373d76dccb02a46287d147a054008bc70
nicsylvia <nicsylvia15f@gmail.com>
2023-03-20 18:49:38 -0700

    working on app config

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from "express";
+
+import cors from "cors"
+
+import morgan from "morgan"
+
+export const AppConfig = (app: Application) =>{
+    app.use(express.json())
+    app.use(cors())
+    app.use(morgan("dev"))
+}
