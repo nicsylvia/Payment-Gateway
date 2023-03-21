201b820d9f18c5517ef485960541711965286f29
nicsylvia <nicsylvia15f@gmail.com>
2023-03-20 19:20:45 -0700

    saved interfaces

---
+++ src/AllInterfaces/Interfaces.ts
@@ -0,0 +1,23 @@
+export interface clientDetails {
+    name: string;
+    address: string;
+    clientType: string;
+    email: string;
+    phoneNumber: number;
+    password: string;
+    notification: []; //pushing the message model inside
+    message: {}[];
+    paymentLog: {}[];
+    contact_us: {}[];
+    bills: {}[];
+  }
+  
+  export interface adminDetails {
+    name: string;
+    email: string;
+    password: string;
+    message: {}[];
+    notification: [];
+    paymentLog: {}[];
+    bills: {}[];
+  }
