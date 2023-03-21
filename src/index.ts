e1960366c789b765d2e283b0c75cd71a227c0e2f
nicsylvia <nicsylvia15f@gmail.com>
2023-03-20 18:45:27 -0700

    saved my server created

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
 
 import {EnvironmentVariables} from "../Config/EnvironmentVariables"
 
 const port = EnvironmentVariables.port
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) =>{
+    return res.status(200).json({
+        message: "API READY FOR GIFT CARD IDEA CONSUMPTION"
+    })
+})
