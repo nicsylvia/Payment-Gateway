55c0c7cad32347e831ec0da76e68f9ae1cb1c075
nicsylvia <nicsylvia15f@gmail.com>
2023-03-22 15:07:29 -0700

    saved gift card routes

--- src/Routes/GiftCardRoutes.ts
+++ src/Routes/GiftCardRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { AllGiftCards, GenerateAGiftCard } from "../Controllers/GiftCardController";
+import { AllGiftCards, BusinessGiftCard, GenerateAGiftCard } from "../Controllers/GiftCardController";
 
 const GiftCardRoutes = express.Router();
 
 GiftCardRoutes.route("generateyourgiftcard/:businessID").post(GenerateAGiftCard)
 GiftCardRoutes.route("/getallgiftcards").get(AllGiftCards)
+GiftCardRoutes.route("/businessgiftcard/:businessID").get(BusinessGiftCard)
 
 export default GiftCardRoutes
