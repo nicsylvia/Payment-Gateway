fcb3964f8e95657fd67e062f3a8be3f00187eebb
nicsylvia <nicsylvia15f@gmail.com>
2023-03-27 07:28:32 -0700

    sved searched gift card

--- src/Routes/GiftCardRoutes.ts
+++ src/Routes/GiftCardRoutes.ts
@@ -1,10 +1,18 @@
 import express from "express";
-import { AllGiftCards, BusinessGiftCard, GenerateAGiftCard } from "../Controllers/GiftCardController";
+import {
+  AllGiftCards,
+  BusinessGiftCard,
+  GenerateAGiftCard,
+  SearchForGiftCard,
+} from "../Controllers/GiftCardController";
 
 const GiftCardRoutes = express.Router();
 
-GiftCardRoutes.route("/generateyourgiftcard/:businessID").post(GenerateAGiftCard)
-GiftCardRoutes.route("/getallgiftcards").get(AllGiftCards)
-GiftCardRoutes.route("/businessgiftcard/:businessID").get(BusinessGiftCard)
+GiftCardRoutes.route("/generateyourgiftcard/:businessID").post(
+  GenerateAGiftCard
+);
+GiftCardRoutes.route("/getallgiftcards").get(AllGiftCards);
+GiftCardRoutes.route("/businessgiftcard/:businessID").get(BusinessGiftCard);
+GiftCardRoutes.route("/searchforgiftcard").post(SearchForGiftCard);
 
-export default GiftCardRoutes
+export default GiftCardRoutes;
